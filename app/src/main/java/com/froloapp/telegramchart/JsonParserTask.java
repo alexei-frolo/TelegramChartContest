9675f7c9187bb6a54fe2086462afc2d8b44a34b7
Alexei Artimovich <a.artimovich@itos.org>
2019-03-13 14:50:53 +0300

    refactored

--- app/src/main/java/com/froloapp/telegramchart/JsonParserTask.java
+++ app/src/main/java/com/froloapp/telegramchart/JsonParserTask.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
 /**
  * Reads a json string from an input stream and parses it into chart data
  */
